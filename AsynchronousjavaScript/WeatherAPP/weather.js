35c868d0481f251e33e2e3a50c1acd30a37a2c83
karishma <kkunwar211@gmail.com>
2020-04-03 11:38:09 +0545

    Added code for weatherApp

--- AsynchronousjavaScript/WeatherAPP/weather.js
+++ AsynchronousjavaScript/WeatherAPP/weather.js
@@ -2,7 +2,7 @@
 
 
 //let's select the classes that we need to modify using queryselector
-const notificationElement = document.querySelector(".notifications");    //for notifications
+const notificationElement = document.querySelector('notifications');    //for notifications
 
 const iconElement = document.querySelector('.weather-icon');            //for weather icons
 
@@ -10,8 +10,15 @@ const tempElement = document.querySelector('.temperature-value p');      //for t
 
 const description = document.querySelector('.temperature-description p');     //temperature description (clear sky or dull)
 
-const location = document.querySelector('.location p');                        //where is the actual location you are residing in
+var location = document.querySelector('.location p');                        //where is the actual location you are residing in
 
+//app data
+const weather = {
+
+}
+weather.temperature ={
+    unit : "celsius"
+}
 
 
 //app constant and var 
@@ -20,10 +27,45 @@ const KELVIN = 273;
 // API KEY
 const key = "82005d27a116c2880c8f0fcb866998a0";
 
-function getWeather(longitude,latitude){
-    
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&long=${longitude}&appid=${key}`;
+//check if your browser supports geolocation or not
+
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition,showError);
 }
+else{
+notificationElement.style.display = "block";
+notificationElement.innerHTML = "<p> Browser doesn't support geoloaction</p>"
+}
+
+//describe the setposition function
+
+function setPosition(position){
+
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+     getWeather(longitude,latitude);
+
+}
+
+function showError(error){
+
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p>${error.message}</p>`;
+}
+
+
+//crate the function get weather
+
+function getWeather(latitude,longitude){
+    
+    
+        let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&long=${longitude}&appid=${key}`;
+    
+    
+
+
+
 
 //to send the request we are going to use fetch
 
@@ -32,25 +74,28 @@ fetch(api).then(function(response){
     let data = response.json();
     return data;
 })
+.then(function(data){
+    weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+    weather.description = data.weather[0].description;
+    weather.iconId = data.weather[0].icon;
+    weather.city = data.name;
+    weather.country = data.sys.country;
+})
+.then(function(){
+    displayWeather();
+});
 
-//It's time to store the temperature data that we actually want to display so why no place it in an object
-/*
-const weather = {
+}//It's time to store the temperature data that we actually want to display so why no place it in an object
 
-    temperature :{
-        value: 18,
-        unit: "celcius"
-    },
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    description.innerHTML = weather.description;
+    location.innerHTML = `${weather.city}, ${weather.country}`;
+}
 
-    description :"few clouds",
-    iconId : "01d",
-    city :"london",
-    country:"GB"
-};
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
 
-//to display the weather we just need to change the innerHTML of each element
-iconElement 
-tempElement
-description
-location
-*/
+
