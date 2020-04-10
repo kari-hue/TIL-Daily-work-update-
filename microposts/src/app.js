58c217c2425f817b7349d1ed630c9c5a45048a44
karishma <kkunwar211@gmail.com>
2020-04-10 11:49:05 +0545

    Added code for removing the posts

--- microposts/src/app.js
+++ microposts/src/app.js
@@ -10,6 +10,13 @@ import { http } from './http';
 import {ui} from './ui';
 
 document.addEventListener('DOMContentLoaded',getPosts);
+//listen for add post
+document.querySelector('.post-submit').addEventListener('click',addPost);
+
+//listen for delete
+ document.querySelector('#posts').addEventListener('click',deletePost);
+
+//getting post
 
 function getPosts(){
 
@@ -20,3 +27,40 @@ function getPosts(){
     
 };
 
+//add post
+
+function addPost(){
+
+    const title = document.querySelecter('#title').value;
+    const body = document.querySelector('#body').value;
+
+const data ={
+    title,
+    body
+}
+
+
+http.post('http://localhost:3000/posts',data)
+.then(data => {
+getPosts(); //get the new post along with the added post
+})
+.catch(err => console.log(err));
+
+}
+
+//delete post
+function deletePost(e){
+    e.preventDefault();
+    if(e.target.parentElement.classList.contains('delete')){
+        const id = e.target.parentElement.dataset.id;
+        if(confirm('ARe you sure')){
+            http.delete(`http://localhost:3000/posts/${id}`)
+            .then(data => {
+                getPosts();
+            })
+            .catch(err => console.log(err));
+
+        }
+    }
+
+}
